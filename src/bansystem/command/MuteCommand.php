572ff2d65a3cf71314c415a4f5f0ea2fdf296a8d
Lyco Tatierra <lycol50@gmail.com>
2021-02-28 22:24:00 +0800

    Added CortexDiscord API

--- src/bansystem/command/MuteCommand.php
+++ src/bansystem/command/MuteCommand.php
@@ -7,6 +7,9 @@ use bansystem\translation\Translation;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
+use CortexPE\DiscordWebhookAPI\Message;
+use CortexPE\DiscordWebhookAPI\Webhook;
+use CortexPE\DiscordWebhookAPI\Embed;
 
 class MuteCommand extends Command {
     
@@ -18,6 +21,8 @@ class MuteCommand extends Command {
     }
     
     public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        $webHook = new Webhook("YOUR WEBHOOK URL");
+        $embed = new Embed();
         if ($this->testPermissionSilent($sender)) {
             if (count($args) <= 0) {
                 $sender->sendMessage(Translation::translateParams("usage", array($this)));
@@ -34,9 +39,19 @@ class MuteCommand extends Command {
                     $muteList->addBan($player->getName(), null, null, $sender->getName());
                     $sender->getServer()->broadcastMessage(TextFormat::AQUA . $player->getName() . TextFormat::RED . " has been muted by $sender.");
                     $player->sendMessage(TextFormat::RED . "You have been muted from our network\n§4Muted by: §bStaff.");
+                    $embed->setTitle("Muted");
+                    $embed->setDescription($player->getName . " has been muted by " . $sender . " !");
+                    $embed->setFooter("AdvancedBan for PMnS","https://cdn.discordapp.com/attachments/784812448535674889/815586272180830248/pmnsoldlogo.jpg");
+                    $msg->addEmbed($embed);
+                    $webHook->send($msg);
                 } else {
                     $muteList->addBan($args[0], null, null, $sender->getName());
                     $sender->getServer()->broadcastMessage(TextFormat::AQUA . $args[0] . TextFormat::RED . " has been muted from our network!\n§4Muted by: §bStaff.");
+                    $embed->setTitle("Muted");
+                    $embed->setDescription($args[0] . " has been muted");
+                    $embed->setFooter("AdvancedBan for PMnS","https://cdn.discordapp.com/attachments/784812448535674889/815586272180830248/pmnsoldlogo.jpg");
+                    $msg->addEmbed($embed);
+                    $webHook->send($msg);
                 }
             } else if (count($args) >= 2) {
                 $reason = "";
@@ -49,9 +64,19 @@ class MuteCommand extends Command {
                     $muteList->addBan($player->getName(), $reason, null, $sender->getName());
                     $sender->getServer()->broadcastMessage(TextFormat::AQUA . $player->getName() . TextFormat::RED . " has been muted by $sender Reason: " . TextFormat::AQUA . $reason . TextFormat::RED . ".");
                     $player->sendMessage(TextFormat::RED . "You have been muted from our network!\§4Muted by: §bStaff\n§5Reason: " . TextFormat::AQUA . $reason . TextFormat::RED . ".");
+                    $embed->setTitle("Muted");
+                    $embed->setDescription($player->getName . " has been muted by " . $sender . " for " . $reason . " !");
+                    $embed->setFooter("AdvancedBan for PMnS","https://cdn.discordapp.com/attachments/784812448535674889/815586272180830248/pmnsoldlogo.jpg");
+                    $msg->addEmbed($embed);
+                    $webHook->send($msg);
                 } else {
                     $muteList->addBan($args[0], $reason, null, $sender->getName());
                     $sender->getServer()->broadcastMessage(TextFormat::AQUA . $args[0] . TextFormat::RED . " has been muted from our network\n§4Muted by: §bStaff\n§5Reason: " . TextFormat::AQUA . $reason . TextFormat::RED . ".");
+                    $embed->setTitle("Muted");
+                    $embed->setDescription($args[0] . " has been muted for " . $reason . " !");
+                    $embed->setFooter("AdvancedBan for PMnS","https://cdn.discordapp.com/attachments/784812448535674889/815586272180830248/pmnsoldlogo.jpg");
+                    $msg->addEmbed($embed);
+                    $webHook->send($msg);
                 }
             }
         } else {
